7c1f0e802c334bd41b0d2d71e1806b959859a83a
Zhuravl21 <168045623+Zhuravl21@users.noreply.github.com>
2024-12-18 16:47:29 +0900

    100

---
+++ orders/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from .models import OrderItem
+from .forms import OrderCreateForm
+from cart.cart import Cart
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            if request.user.is_authenticated:
+                order.user = request.user
+            order.save()
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item['product'],
+                    price=item['price'],
+                    quantity=item['quantity']
+                )
+            # Очистка корзины
+            cart.clear()
+            return render(request, 'orders/order_created.html', {'order': order})
+    else:
+        form = OrderCreateForm()
+    return render(request, 'orders/order_create.html', {'cart': cart, 'form': form})
